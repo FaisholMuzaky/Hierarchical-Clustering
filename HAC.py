33d0f1e829ca180558d43a30ee151f319db64264
FaisholMuzaky <fmuzaki@gmail.com>
2019-04-08 00:00:21 +0700

    Add files via upload

---
+++ HAC.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 21:38:18 2019
+
+@author: Asus
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.cluster.hierarchy as sch
+from tkinter.filedialog import askopenfilename
+
+#select file
+filename = askopenfilename()
+
+#read file.csv
+dataset = pd.read_csv(filename)
+X = dataset.iloc[:,[0,1,2,3]].values
+y_set = dataset.iloc[:,[4]].values
+
+#choose the method
+print("=============================")
+print("1. Single Linkage")
+print("2. Complete Linkage")
+print("3. Median Linkage")
+print("4. Average Linkage")
+print("=============================")
+pilih = int(input("input your choice : "))
+if pilih == 1:
+    metod = 'single'
+elif pilih == 2:
+    metod = 'complete'
+elif pilih == 3:
+    metod = 'median'
+elif pilih == 4:
+    metod = 'average'
+else:
+   sys.exit("Sorry, but your choice is wrong")
+
+#create dendogram
+Z = sch.linkage(X, method = metod)
+den = sch.dendrogram(Z)
+
+#show dendogram
+plt.title('Dendrogram for the clustering of the dataset')
+plt.show()
+
